4655387977c4180beac69f57faba39b4e37baeb1
fortis <moosorkh@gmail.com>
2025-12-31 16:32:00 -0800

    Make company name required in registration schema

--- server/validators/authValidator.ts
+++ server/validators/authValidator.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const registerSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
-  companyName: z.string().optional(),
+  companyName: z.string().min(1, "Company name is required"),
 });
 
 export const loginSchema = z.object({

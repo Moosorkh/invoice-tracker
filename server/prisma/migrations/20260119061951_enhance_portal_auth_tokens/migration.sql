0be332754bb45788d6b71c46c3df5b4148cef226
fortis <moosorkh@gmail.com>
2026-01-19 22:28:02 -0800

    Fix migration to handle existing PortalAuthToken rows

--- server/prisma/migrations/20260119061951_enhance_portal_auth_tokens/migration.sql
+++ server/prisma/migrations/20260119061951_enhance_portal_auth_tokens/migration.sql
@@ -1,13 +1,13 @@
-/*
-  Warnings:
-
-  - Added the required column `type` to the `PortalAuthToken` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
+-- First, add the column as nullable
 ALTER TABLE "PortalAuthToken" ADD COLUMN     "portalUserId" TEXT,
-ADD COLUMN     "type" TEXT NOT NULL,
+ADD COLUMN     "type" TEXT,
 ADD COLUMN     "usedAt" TIMESTAMP(3);
+
+-- Update existing rows to have a default type
+UPDATE "PortalAuthToken" SET "type" = 'magic_link' WHERE "type" IS NULL;
+
+-- Now make it NOT NULL
+ALTER TABLE "PortalAuthToken" ALTER COLUMN "type" SET NOT NULL;
 
 -- CreateIndex
 CREATE INDEX "PortalAuthToken_portalUserId_idx" ON "PortalAuthToken"("portalUserId");

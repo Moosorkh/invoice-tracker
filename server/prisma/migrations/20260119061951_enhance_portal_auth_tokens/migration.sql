1ddfdd3f29a0c7711d7b5843b81609df2d1196b8
fortis <moosorkh@gmail.com>
2026-01-19 22:33:17 -0800

    Clear temp tokens before migration

--- server/prisma/migrations/20260119061951_enhance_portal_auth_tokens/migration.sql
+++ server/prisma/migrations/20260119061951_enhance_portal_auth_tokens/migration.sql
@@ -1,13 +1,10 @@
--- First, add the column as nullable
+-- Delete existing tokens (they're temporary and expire anyway)
+DELETE FROM "PortalAuthToken";
+
+-- AlterTable
 ALTER TABLE "PortalAuthToken" ADD COLUMN     "portalUserId" TEXT,
-ADD COLUMN     "type" TEXT,
+ADD COLUMN     "type" TEXT NOT NULL,
 ADD COLUMN     "usedAt" TIMESTAMP(3);
-
--- Update existing rows to have a default type
-UPDATE "PortalAuthToken" SET "type" = 'magic_link' WHERE "type" IS NULL;
-
--- Now make it NOT NULL
-ALTER TABLE "PortalAuthToken" ALTER COLUMN "type" SET NOT NULL;
 
 -- CreateIndex
 CREATE INDEX "PortalAuthToken_portalUserId_idx" ON "PortalAuthToken"("portalUserId");
